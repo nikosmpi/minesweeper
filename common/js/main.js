25d6291f0ee015b2e9704e920523354117a758ac
Nikos Mpilalis <nikos.mpilalis@mozaik.com>
2022-03-04 23:28:05 +0200

    flood

--- common/js/main.js
+++ common/js/main.js
@@ -5,41 +5,41 @@ function initMines() {
 			el: appElement,
 			data: {
 				tempDimension: 0,
-				mines: 10,
+				mines: 25,
 				gridDimension: 10,
-				grid: []
+				grid: [],
+				gameOver: false
 			},
 			created() {
 				this.putMines();
 			},
 			computed: {
-				
+				youWin() {
+					const checkBlocks = [];
+					for(let g of this.grid){
+						if(!g.revealed) {
+							checkBlocks.push(true);
+						}
+					}
+					return checkBlocks.length === this.mines;
+				}
 			},
 			methods: {
+				resetGame() {
+					this.putMines();
+					this.gameOver = false;
+				},
 				cellTextShow(i,j) {
 					const selected = this.grid.findIndex(g => i === g.i && j === g.j);
 					return this.grid[selected].revealed;
-				},
+				},				
 				cellText(i,j){
 					const selected = this.grid.findIndex(g => i === g.i && j === g.j);
-					const minecount = [];
-					const fromI = i - 1 === 0 ? i : i - 1;
-					const toI = i + 1 === 11 ? i : i + 1;
-					const fromJ = j - 1 === 0 ? j : j - 1;
-					const toJ = j + 1 === 11 ? j : j + 1;
-					for(let l = fromI; l <= toI; l++) {
-						for(let k = fromJ; k <= toJ; k++) {
-							const selected = this.grid.findIndex(g => l === g.i && k === g.j);
-							if(this.grid[selected].mine) {
-								minecount.push(true);
-							}
-						}
-					}
-					return !this.grid[selected].mine ? minecount.length : null;
+					return this.grid[selected].number !== -1 ? this.grid[selected].number : null;
 				},
 				cellInfo(i,j) {
 					const selected = this.grid.findIndex(g => i === g.i && j === g.j);
-					const isMine = this.grid[selected].mine;
+					const isMine = this.grid[selected].number === -1;
 					const isRevealed = this.grid[selected].revealed;
 					return {
 							'mine': isMine && isRevealed,
@@ -53,7 +53,31 @@ function initMines() {
 				},
 				clickCell(i,j) {
 					const selected = this.grid.findIndex(g => i === g.i && j === g.j);
-					this.grid[selected].revealed = true;
+					if(this.grid[selected].number === -1) {
+						this.grid[selected].revealed = true;
+						this.gameOver = true;
+					} else if(this.grid[selected].number === 0) {
+						this.floodCell(selected);
+					} else {
+						this.grid[selected].revealed = true;
+					}
+				},
+				floodCell(thisSelected) {
+					const i = this.grid[thisSelected].i;
+					const j = this.grid[thisSelected].j;
+					const fromI = i - 1 === 0 ? i : i - 1;
+					const toI = i + 1 === this.gridDimension + 1 ? i : i + 1;
+					const fromJ = j - 1 === 0 ? j : j - 1;
+					const toJ = j + 1 === this.gridDimension + 1 ? j : j + 1;
+					for(let l = fromI; l <= toI; l++) {
+						for(let k = fromJ; k <= toJ; k++) {
+							const selected = this.grid.findIndex(g => l === g.i && k === g.j);
+							if(this.grid[selected].number >= 0 && !this.grid[selected].revealed) {
+								this.grid[selected].revealed = true;
+								this.floodCell(selected);
+							}
+						}
+					}	
 				},
 				putMines() {
 					const mines = [];
@@ -71,14 +95,30 @@ function initMines() {
 					for (let i = 1; i <= this.gridDimension; i++) {
 						for (let j = 1; j <= this.gridDimension; j++) {
 							this.grid.push({
-								i, j, mine: false, revealed: false
+								i, j, revealed: false, number: 0
 							});
 						}
 					}
 					for(let mine of mines) {
-						this.grid[mine].mine = true;
+						this.grid[mine].number = -1;
 					}
-					
+					for (let i = 1; i <= this.gridDimension; i++) {
+						for (let j = 1; j <= this.gridDimension; j++) {
+							const thisMine = this.grid.findIndex(g => i === g.i && j === g.j);
+							const fromI = i - 1 === 0 ? i : i - 1;
+							const toI = i + 1 === this.gridDimension + 1 ? i : i + 1;
+							const fromJ = j - 1 === 0 ? j : j - 1;
+							const toJ = j + 1 === this.gridDimension + 1 ? j : j + 1;
+							for(let l = fromI; l <= toI; l++) {
+								for(let k = fromJ; k <= toJ; k++) {
+									const selected = this.grid.findIndex(g => l === g.i && k === g.j);
+									if(this.grid[selected].number === -1 && this.grid[thisMine].number !== -1) {
+										this.grid[thisMine].number = this.grid[thisMine].number + 1;
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 		});
